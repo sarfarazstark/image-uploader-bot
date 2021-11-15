b541b8b101cc409cfe61d457089290c9e62e3492
Sarfaraz Alam <71469354+sarfarazstark@users.noreply.github.com>
2021-11-15 19:13:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Image-Upload-Bot.git /Image-Upload-Bot
+git clone https://github.com/sarfarazstark/image-uploader-bot.git /image-uploader-bot
 cd /Image-Upload-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
